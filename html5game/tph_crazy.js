be3895ca4e8d325ce67adf4ba4ee892af46447a2
lilShake <abhishek.sensharma.2013@gmail.com>
2021-12-25 18:43:50 +0530

    Domain ready

--- html5game/tph_crazy.js
+++ html5game/tph_crazy.js
@@ -1,9 +1,17 @@
 var b = 0;
-
+var madStarted = 0
 
 function crazyStep() {
     if (b > 0) {
         b--;
+        var c = "yes";
+    } else var c = "no";
+    return c;
+}
+
+function MadStep() {
+    if (madStarted > 0) {
+        madStarted--;
         var c = "yes";
     } else var c = "no";
     return c;
@@ -16,17 +24,45 @@ function start() {
 
 }
 
-function RadFinished() {
-    b++;
-    crazysdk.removeEventListener('adFinished', window.RadFinished);
+function MadStarted() {
+    madStarted++;
+    crazysdk.removeEventListener('adStarted', window.MadStarted);
 }
 
+function MadError() {
+    // c++;
+    crazysdk.removeEventListener('adError', window.MadError);
+}
+
+function RadError() {
+    // c++;
+    crazysdk.removeEventListener('adError', window.RadError);
+}
+
+
+
+
+
+
+function RadStarted() {
+    b++;
+    crazysdk.removeEventListener('adStarted', window.RadStarted);
+}
+
+
+
+
+
+// MIDGAME AND REWARD CALL CODE
 function crazyAd_mid() {
     window.crazysdk.requestAd("midgame");
+    crazysdk.addEventListener('adStarted', window.MadStarted);
+    crazysdk.addEventListener('adError', window.MadError);
 
 }
 
 function crazyAd_reward() {
     window.crazysdk.requestAd("rewarded");
-    crazysdk.addEventListener('adFinished', window.RadFinished);
+    crazysdk.addEventListener('adStarted', window.RadStarted);
+    crazysdk.addEventListener('adError', window.RadError);
 }
